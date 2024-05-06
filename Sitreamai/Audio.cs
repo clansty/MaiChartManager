61dcad45817ec71af5b76d7f07f0aff7a14393ed
Clansty <i@gao4.pw>
2024-05-06 20:51:51 +0800

    embed template

--- Sitreamai/Audio.cs
+++ Sitreamai/Audio.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using NAudio.Wave;
 using VGAudio;
 using VGAudio.Cli;
@@ -10,13 +11,27 @@ namespace Sitreamai
     {
         public static void ConvertToMai(string srcPath, string savePath)
         {
+            var wrapper = new ACB_Wrapper(ACB_File.Load(ReadResourceFile("Sitreamai.Resources.template.acb"), null));
             var trackBytes = LoadAndConvertFile(srcPath, FileType.Hca, false, 9170825592834449000);
-            var wrapper = new ACB_Wrapper(ACB_File.Load(@"D:\Maimai HDD\A500\template.acb"));
 
             wrapper.Cues[0].AddTrackToCue(trackBytes, true, false, EncodeType.HCA);
             wrapper.AcbFile.Save(savePath);
         }
-        
+
+        private static byte[] ReadResourceFile(string filename)
+        {
+            using var s = Assembly.GetExecutingAssembly().GetManifestResourceStream(filename);
+            var buffer = new byte[1024];
+            using var ms = new MemoryStream();
+            while (true)
+            {
+                var read = s.Read(buffer, 0, buffer.Length);
+                if (read <= 0)
+                    return ms.ToArray();
+                ms.Write(buffer, 0, read);
+            }
+        }
+
         public static byte[] LoadAndConvertFile(string path, FileType convertToType, bool loop, ulong encrpytionKey = 0)
         {
             switch (Path.GetExtension(path).ToLower())
@@ -43,7 +58,7 @@ namespace Sitreamai
 
             throw new InvalidDataException($"Filetype of \"{path}\" is not supported.");
         }
-        
+
         public static byte[] ConvertToWav(string path)
         {
             byte[] outBytes;
@@ -59,8 +74,9 @@ namespace Sitreamai
 
             return outBytes;
         }
-        
-        public static byte[] ConvertFile(byte[] bytes, FileType encodeType, FileType convertToType, bool loop, ulong encryptionKey = 0)
+
+        public static byte[] ConvertFile(byte[] bytes, FileType encodeType, FileType convertToType, bool loop,
+            ulong encryptionKey = 0)
         {
             ConvertStatics.SetLoop(loop, 0, 0);
 
@@ -70,7 +86,7 @@ namespace Sitreamai
                 options.KeyCode = encryptionKey;
                 options.Loop = loop;
 
-                if(options.Loop)
+                if (options.Loop)
                     options.LoopEnd = int.MaxValue;
 
                 byte[] track = ConvertStream.ConvertFile(options, ms, encodeType, convertToType);
@@ -81,5 +97,5 @@ namespace Sitreamai
                 return track;
             }
         }
-    }    
+    }
 }
