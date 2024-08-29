4bf269fea84bdc5e1e098f497fce9dc9a7d62fcc
Clansty <i@gao4.pw>
2024-08-29 17:47:21 +0800

    make appx

---
+++ Packaging/make.cmd
@@ -0,0 +1,11 @@
+set PATH=%PATH%;"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64";"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64"
+
+pushd Pack
+
+del .\priconfig.xml
+del .\*.pri
+makepri.exe createconfig /cf priconfig.xml /dq zh-CN
+makepri.exe new /pr . /cf .\priconfig.xml
+del .\priconfig.xml
+makeappx pack /d . /p ../Store64.appx
+del .\*.pri
