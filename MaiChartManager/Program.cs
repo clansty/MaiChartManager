cd14b6063169303f73d1eadafc52d4e5061935f3
Clansty <i@gao4.pw>
2024-09-03 11:48:50 +0800

    chore: 启动过程中的错误处理

--- MaiChartManager/Program.cs
+++ MaiChartManager/Program.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using System.Windows.Forms;
 
 namespace MaiChartManager;
 
@@ -11,26 +10,34 @@ static class Program
     [STAThread]
     static void Main()
     {
-        SentrySdk.Init(o =>
-            {
-                // Tells which project in Sentry to send events to:
-                o.Dsn = "https://be7a9ae3a9a88f4660737b25894b3c20@sentry.c5y.moe/3";
-                // Set TracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-                // We recommend adjusting this value in production.
-                o.TracesSampleRate = 0.5;
-            }
-        );
+        try
+        {
+            SentrySdk.Init(o =>
+                {
+                    // Tells which project in Sentry to send events to:
+                    o.Dsn = "https://be7a9ae3a9a88f4660737b25894b3c20@sentry.c5y.moe/3";
+                    // Set TracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+                    // We recommend adjusting this value in production.
+                    o.TracesSampleRate = 0.5;
+                }
+            );
 
-        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
-        ApplicationConfiguration.Initialize();
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
+            ApplicationConfiguration.Initialize();
 
-        Directory.CreateDirectory(StaticSettings.appData);
-        Directory.CreateDirectory(StaticSettings.tempPath);
-        if (File.Exists(Path.Combine(StaticSettings.appData, "config.json")))
-            StaticSettings.Config = JsonSerializer.Deserialize<Config>(File.ReadAllText(Path.Combine(StaticSettings.appData, "config.json")));
+            Directory.CreateDirectory(StaticSettings.appData);
+            Directory.CreateDirectory(StaticSettings.tempPath);
+            if (File.Exists(Path.Combine(StaticSettings.appData, "config.json")))
+                StaticSettings.Config = JsonSerializer.Deserialize<Config>(File.ReadAllText(Path.Combine(StaticSettings.appData, "config.json")));
 
-        new Launcher().Show();
+            new Launcher().Show();
 
-        Application.Run();
+            Application.Run();
+        }
+        catch (Exception e)
+        {
+            SentrySdk.CaptureException(e);
+            MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
