26a2e111449c93695694f8cf3f2a81ed04e230b0
Clansty <i@gao4.pw>
2024-09-07 01:03:10 +0800

    bump: 增加版本号

--- MaiChartManager/Properties/AssemblyInfo.cs
+++ MaiChartManager/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("MaiChartManager")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyCompany("Clansty")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyInformationalVersion("1.0.4")]
 [assembly: AssemblyProduct("MaiChartManager")]
 [assembly: AssemblyTitle("MaiChartManager")]
-[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
