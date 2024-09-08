c135cefb686dc4e41c0def351a7401eec2b357d9
Clansty <i@gao4.pw>
2024-09-08 19:20:08 +0800

    chore: 修改主窗口大小

--- MaiChartManager/Browser.Designer.cs
+++ MaiChartManager/Browser.Designer.cs
@@ -35,9 +35,9 @@ partial class Browser
         webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
         ((ISupportInitialize)webView21).BeginInit();
         SuspendLayout();
-        // 
+        //
         // webView21
-        // 
+        //
         webView21.AllowExternalDrop = true;
         webView21.CreationProperties = null;
         webView21.DefaultBackgroundColor = System.Drawing.Color.White;
@@ -48,12 +48,12 @@ partial class Browser
         webView21.TabIndex = 0;
         webView21.ZoomFactor = 1D;
         webView21.CoreWebView2InitializationCompleted += webView21_CoreWebView2InitializationCompleted;
-        // 
+        //
         // Browser
-        // 
+        //
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(1782, 1253);
+        ClientSize = new System.Drawing.Size(2000, 1253);
         Controls.Add(webView21);
         Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
         Name = "Browser";
