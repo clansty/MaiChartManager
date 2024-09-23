97bffa58fc27f7978db321b586e5171c08897490
Clansty <i@gao4.pw>
2024-09-23 17:53:05 +0800

    bump: version

--- MaiChartManager/Properties/AssemblyInfo.cs
+++ MaiChartManager/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Clansty")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
 [assembly: AssemblyProduct("MaiChartManager")]
 [assembly: AssemblyTitle("MaiChartManager")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0.17763.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0.14393.0")]
