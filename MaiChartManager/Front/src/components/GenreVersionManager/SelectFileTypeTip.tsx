2c482ae81b29fbdbba5825d22cf63300d428145d
Clansty <i@gao4.pw>
2024-09-29 13:35:20 +0800

    bump: mod + version

--- MaiChartManager/Front/src/components/GenreVersionManager/SelectFileTypeTip.tsx
+++ MaiChartManager/Front/src/components/GenreVersionManager/SelectFileTypeTip.tsx
@@ -11,7 +11,7 @@ export default defineComponent({
     return () => <NDrawer show={props.show} height={250} placement="bottom">
       <NDrawerContent title="可以选择的文件类型">
         <NFlex vertical size="large">
-          图片分辨率和比例无所谓，但是最好是差不多那个比例的横向透明 PNG 图片
+          图片比例无所谓，但是最好是差不多那个比例的横向透明 PNG 图片。分辨率在 332x160 左右最好。
           <div class="grid cols-4 justify-items-center text-8em gap-10">
             <FileTypeIcon type="JPG"/>
             <FileTypeIcon type="PNG"/>
