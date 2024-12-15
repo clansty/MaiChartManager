7e79d8f532f32171d4107faa8f6b978bf397d480
Clansty <i@gao4.pw>
2024-12-15 22:10:19 +0800

    fix: 主界面打开的时候不需要登录

--- MaiChartManager/AuthenticationMiddleware.cs
+++ MaiChartManager/AuthenticationMiddleware.cs
@@ -11,7 +11,7 @@ public class AuthenticationMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        if (!context.User.Identity.IsAuthenticated)
+        if (!context.User.Identity.IsAuthenticated && context.Connection.LocalPort == 5001)
         {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             context.Response.Headers.Append("WWW-Authenticate", "Basic");
