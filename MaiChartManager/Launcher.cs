bb614b06e70bdbc235ffa6ac5c4781b9f4a446c9
Clansty <i@gao4.pw>
2025-07-20 16:28:23 +0800

    bump mailib

--- MaiChartManager/Launcher.cs
+++ MaiChartManager/Launcher.cs
@@ -29,7 +29,7 @@ public partial class Launcher : Form
 # if DEBUG
         checkBox1.Checked = true;
         StaticSettings.Config.Export = true;
-        textBox1.Text = @"M:\Package";
+        textBox1.Text = @"H:\Maimai\EZ156 Debug\Package";
         StartClicked(null, null);
         notifyIcon1.Visible = true;
         WindowState = FormWindowState.Minimized;
